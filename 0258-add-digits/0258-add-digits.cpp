afcb3e20d5521076fa318c95cdc74f2e024831dd
Ravi Kumar <ravikumarstm333@gmail.com>
2026-08-29 11:26:23 +0530

    Time: 3 ms (3.44%), Space: 8.7 MB (7.61%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+
+        while(num>=10){
+            int res=0;
+            while(num!=0){
+                res+=num%10;
+                num/=10;
+            }
+            num=res;
+
+        }
+        return num;
+        
+    }
+};
