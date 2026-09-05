b091b7e7adb2d67e0a30cb02112b458644cb2f6b
Ravi Kumar <ravikumarstm333@gmail.com>
2026-09-05 22:52:53 +0530

    Time: 9 ms (63.62%), Space: 202.4 MB (98.43%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -3,20 +3,16 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         // TC o(n) 
         // SC o(n)
-        
-        map<int,pair<int,int>> mp;
+        vector<int> minInde(nums.size());
         int maxe=nums[0];
         int mine=nums[nums.size()-1];
-        for(int i=0;i<nums.size();i++){
-            maxe=max(maxe,nums[i]);
-            mp[i].first=maxe;
-        }
         for(int i=nums.size()-1;i>=0;i--){
-            mine=min(mine,nums[i]);
-            mp[i].second=mine;
+            if(nums[i]<mine)mine=nums[i];
+            minInde[i]=mine;
         }
         for(int i=0;i<nums.size();i++){
-            if((mp[i].first-mp[i].second)<=k)return i;
+            if(nums[i]>maxe)maxe=nums[i];
+            if((maxe-minInde[i])<=k)return i;
         }
         return -1;
         
