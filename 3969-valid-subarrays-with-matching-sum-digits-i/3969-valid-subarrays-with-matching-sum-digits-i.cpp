dc117915c98fabba822ed411079375c6190de6f4
Ravi Kumar <ravikumarstm333@gmail.com>
2026-09-06 17:41:57 +0530

    Time: 166 ms (65.7%), Space: 32.4 MB (53.63%) - LeetHub

---
+++ 3969-valid-subarrays-with-matching-sum-digits-i/3969-valid-subarrays-with-matching-sum-digits-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool chelFirstAndLast(long long num,int x){
+        if(num==x) return true;
+        if(x!=num%10)return false;
+        while(num>0){
+            int d=num%10;
+            num/=10;
+            if(num==x){
+                return true;
+            }
+        }
+        return false;
+    }
+    int countValidSubarrays(vector<int>& nums, int x) {
+        int res=0;
+        for(int i=0;i<nums.size();i++){
+            long long sum=0;
+            for(int j=i;j<nums.size();j++){
+                sum+=nums[j];
+                if(chelFirstAndLast(sum,x)){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+};
