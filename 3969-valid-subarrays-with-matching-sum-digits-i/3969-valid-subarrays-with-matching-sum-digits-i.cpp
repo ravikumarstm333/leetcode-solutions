63f66b6037197ebaae6cd80df7513e7a58958ce0
Ravi Kumar <ravikumarstm333@gmail.com>
2026-09-06 17:59:46 +0530

    Time: 157 ms (71.13%), Space: 32.4 MB (53.63%) - LeetHub

--- 3969-valid-subarrays-with-matching-sum-digits-i/3969-valid-subarrays-with-matching-sum-digits-i.cpp
+++ 3969-valid-subarrays-with-matching-sum-digits-i/3969-valid-subarrays-with-matching-sum-digits-i.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool chelFirstAndLast(long long num,int x){
-        // if(num==x) return true;
-        // if(x!=num%10)return false;
-        // while(num>0){
-        //     int d=num%10;
-        //     num/=10;
-        //     if(num==x){
-        //         return true;
-        //     }
-        // }
-        // return false;
-        int last=num%10;
-        int first=num/pow(10,(int)log10(num));
-        if(x==last && first==x) return true;
+        if(num==x) return true;
+        if(x!=num%10)return false;
+        while(num>0){
+            int d=num%10;
+            num/=10;
+            if(num==x){
+                return true;
+            }
+        }
         return false;
 
 
