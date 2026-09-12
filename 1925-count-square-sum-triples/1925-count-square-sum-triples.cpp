ece8ba6ce89f3812433a8932866d322107395d4a
Ravi Kumar <ravikumarstm333@gmail.com>
2026-09-13 02:35:43 +0530

    Time: 6 ms (77.66%), Space: 7.9 MB (17.45%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int countTriples(int n) {
-        // TC O(n^3)
+        // TC O(n^2)
         // SC O(1)
         int res=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                for(int k=1;k<=n;k++){
-                    if(i*i+j*j==k*k)res++;
-                }
+                int a=i*i+j*j;
+                int c=sqrt(a);
+                if(c<=n && c*c==a)res++;
             }
         }
         return res;
