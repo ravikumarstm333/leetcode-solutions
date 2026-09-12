07d47a92b28a4b9e485522af218764772eef53de
Ravi Kumar <ravikumarstm333@gmail.com>
2026-09-13 02:16:45 +0530

    Time: 6 ms (45.05%), Space: 11.1 MB (90.1%) - LeetHub

--- 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.cpp
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int unequalTriplets(vector<int>& nums) {
-        // TC O(n)
+        // TC O(n^3)
         // SC O(1)
         int res=0;
         for(int i=0;i<nums.size();i++){
