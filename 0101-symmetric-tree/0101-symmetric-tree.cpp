a11b50ced93c0c4a78ad8feea20c34875ab85baa
Ravi Kumar <ravikumarstm333@gmail.com>
2026-09-17 17:17:30 +0530

    Time: 0 ms (100%), Space: 18.4 MB (73.18%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,34 +11,13 @@
  */
 class Solution {
 public:
+    bool helper(TreeNode* left,TreeNode* right){
+        if(left==nullptr && right==nullptr ) return true;
+        if(left==nullptr || right==nullptr) return false;
+        if(left->val!=right->val) return false;
+        return helper(left->left,right->right) && helper(left->right,right->left);
+    }
     bool isSymmetric(TreeNode* root) {
-        queue<TreeNode*> qu;
-        qu.push(root);
-        while(!qu.empty()){
-            int n=qu.size();
-            vector<int> v;
-            int a=0;
-            for(int i=0;i<n;i++){
-                TreeNode* node=qu.front();
-                qu.pop();
-                if(node->left!=nullptr){
-                    qu.push(node->left);
-                    v.push_back(node->left->val);
-                    a=1;
-                }else v.push_back(101);
-                if(node->right!=nullptr){
-                    qu.push(node->right);
-                    v.push_back(node->right->val);
-                    a=1;
-                }else v.push_back(101);
-            }
-            if(a==0)break;
-            for(int i=0;i<v.size()/2;i++){
-                if(v[i]!=v[v.size()-1-i]) return false;
-            }
-        }
-        return true;
-
-
+      return root==nullptr || helper(root->left,root->right);  
     }
 };
