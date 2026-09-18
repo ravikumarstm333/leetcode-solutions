90f56f621b3286b85131e0b96ef69df268839dee
Ravi Kumar <ravikumarstm333@gmail.com>
2026-09-19 00:45:06 +0530

    Time: 0 ms (100%), Space: 15.2 MB (51.59%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,46 +1,12 @@
 class Solution {
 public:
-    void MergeArray(vector<int>&arr,int l,int m,int r){
-        int i=l;
-        int j=m+1;
-        vector<int> v;
-        while(i<=m && j<=r){
-            if(arr[i]<=arr[j]){
-                v.push_back(arr[i]);
-                i++;
-            }else{
-                v.push_back(arr[j]);
-                j++;
-            }
-        }
-        while(i<=m){
-            v.push_back(arr[i]);
-            i++;
-        }
-        while(j<=r){
-            v.push_back(arr[j]);
-            j++;
-        }
-        for(int k=0;k<v.size();k++){
-            arr[k+l]=v[k];
-        }
-    }
-    void MergeSort(vector<int> &nums,int l,int r){
-        if(l>=r)return;
-        int m=(r-l)/2+l;
-        MergeSort(nums,l,m);
-        MergeSort(nums,m+1,r);
-        MergeArray(nums,l,m,r);
-    }
     vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
         vector<int> res;
-        MergeSort(nums,0,nums.size()-1);
         for(int i=0;i<nums.size();i++){
             if(nums[i]==target){
                 res.push_back(i);
-            }else if(res.size()>0){
-                break;
-            }
+            }else if(res.size()>0) break;
         }
         return res;
     }
